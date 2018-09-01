0762dd3ca7d881e92acefdd84207818ae53486bd
Zack Layton <SYNStack@users.noreply.github.com>
2018-09-01 05:29:39 -0400

    Windows spec fixes and CI (#23)
    * Fix: Changed shell test for OS-neutral exitstatus check
    
    * Fix: Added explicit 'ruby' so OS/Windows knows how to run fue
    
    * Fixes for #12 and #13
    
    * Added periods and removed unnecessary entry
    
    * Testing AppVeyor integration
    
    * EnforcedStyle to ensure 'LF' line endings, Appveyor compliance
    
    * Corrected EndOfLine namespace
    
    * Removed legacy versions from environment
    
    * Added newer Ruby versions
    
    * Added Appveyor status badge
    
    * Updated changelog
    
    * Added Appveyor status badge
    
    * Fix: Changed CRLF line endings back to LF
    
    * Cleaned up call to 'ruby', and fixed issue #15
    
    * Trigger a build.
    
    * No need for so many rubies.
    
    * Testing AppVeyor integration
    
    * EnforcedStyle to ensure 'LF' line endings, Appveyor compliance
    
    * Corrected EndOfLine namespace
    
    * Removed legacy versions from environment
    
    * Added newer Ruby versions
    
    * Added Appveyor status badge
    
    * Updated changelog
    
    * Fix: Changed CRLF line endings back to LF
    
    * Forgot to add cache to bundle config

--- spec/fue/shell_spec.rb
+++ spec/fue/shell_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Fue::Shell do
   context '#system!' do
     it 'returns a zero error code' do
-      expect(Fue::Shell.system!('echo OK ; exit 0')).to eq 'OK'
+      expect(Fue::Shell.system!('echo OK')).to eq 'OK'
     end
     it 'returns a non zero error code' do
       expect do
-        Fue::Shell.system!('echo OK ; exit 1')
+        Fue::Shell.system!('exit 1')
       end.to raise_error RuntimeError, /exit code pid \d* exit 1/
     end
   end
